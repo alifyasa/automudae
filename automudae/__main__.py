7b95e7b58ae99d080268ee6682bd2829cf5696c0
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2025-05-25 22:36:59 +0700

    fix: Linting Error

--- automudae/__main__.py
+++ automudae/__main__.py
@@ -2,7 +2,7 @@ from automudae.client import AutoMudaeClient
 from automudae.config.v1 import get_config
 
 
-def main():
+def main() -> None:
     config = get_config()
     client = AutoMudaeClient(config)
     client.run(token=client.config.discord.token, root_logger=True)
