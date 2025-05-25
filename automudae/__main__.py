ccba17984f2e768105198a4b83af74cae8ee7b94
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2025-05-26 06:11:31 +0700

    feat: Generate Schema

--- automudae/__main__.py
+++ automudae/__main__.py
@@ -1,9 +1,15 @@
 from automudae.client import AutoMudaeClient
-from automudae.config.v1 import get_config
+from automudae.config.v1 import get_config, Config
+import yaml
 
 
 def main() -> None:
     config = get_config()
+
+    config_schema = yaml.dump(Config.model_json_schema())
+    with open("config_schema.yaml", "w") as f:
+        f.write(config_schema)
+
     client = AutoMudaeClient(config)
     client.run(token=client.config.discord.token, root_logger=True)
 
