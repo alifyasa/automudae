2c7b8c0fec22ea6c05090721bca244087744dc63
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2025-06-03 13:14:16 +0700

    fix: Remove Args

--- automudae/args.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(
-    prog="Auto Mudae", description="Automated script for Mudae. Requires a config file."
-)
-parser.add_argument(
-    "-f", "--file", type=str, required=True, help="Path to the config file"
-)
