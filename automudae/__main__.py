c3aa8d876b67b0c843fde6ff69262848f429fc3c
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2025-06-03 13:13:12 +0700

    fix: Pylint

--- automudae/__main__.py
+++ automudae/__main__.py
@@ -1,3 +1,9 @@
+"""
+AutoMudae
+
+Automatically Play Mudae
+"""
+
 import yaml
 
 from automudae.agent import AutoMudaeAgent
@@ -6,11 +12,12 @@ from automudae.config import Config
 
 
 def main() -> None:
+    """Entry point for the AutoMudae Discord Bot"""
     args = parser.parse_args()
     config = Config.from_file(path=args.file)
 
     config_schema = yaml.dump(Config.model_json_schema())
-    with open("config/schema.yaml", "w") as f:
+    with open("config/schema.yaml", "w", encoding="utf-8") as f:
         f.write(config_schema)
 
     agent = AutoMudaeAgent(config)
